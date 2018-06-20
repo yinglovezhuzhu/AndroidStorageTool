4083a5e9822b26afc80a3c06c1e3d2503c73c733
zhangyy <zhangyy_owen@139.com>
2018-06-20 17:32:41 +0800

    去除无效的import

--- android-storage-tool/src/main/java/com/xiaoying/storagetool/StorageTool.java
+++ android-storage-tool/src/main/java/com/xiaoying/storagetool/StorageTool.java
@@ -18,9 +18,7 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <br/>Author：yunying.zhang
--- android-storage-tool/src/main/java/com/xiaoying/storagetool/VolumeInfo.java
+++ android-storage-tool/src/main/java/com/xiaoying/storagetool/VolumeInfo.java
@@ -5,8 +5,6 @@ import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.io.File;
-
 /**
  * <br/>Author：yunying.zhang
  * <br/>Email: yunyingzhang@rastar.com
